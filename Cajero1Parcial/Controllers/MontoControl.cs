d76a6d2e39118137b069db42898eea441d7c170e
Jose101503 <ph0172032019@unab.edu.sv>
2021-09-11 17:23:32 -0600

    Agregar archivos de proyecto.

---
+++ Cajero1Parcial/Controllers/MontoControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cajero1Parcial.Controllers
+{
+    public class MontoControl
+    {
+
+        public ActionResult procesar(int Monto)
+        {
+
+            if ((Monto % 5) == 0) { 
+            return RedirectResult("/CajeroVistas/VistaError");
+        }
+            else
+            {
+                return RedirectResult("/CajeroVistas/MontoMostrar");
+            }
+        }
+
+        private ActionResult RedirectResult(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+    
+}
---
+++ Cajero1Parcial/Models/ViewModels/MontoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Cajero1Parcial.Models.ViewModels
+{
+    public class MontoModel
+    {
+        [Display(Name = "Monto")]
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public int Monto { get; set; }
+    }
+    public ActionResult()
+    {
+
+        return ViewModels();
+    }
+}
+
